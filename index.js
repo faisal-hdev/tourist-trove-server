3079843d11bb33f40d02521ca80cc1904b8c3287
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-05-02 04:25:16 +0600

    get the myList data and send them uer email

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
       res.send(result);
     });
 
+    // get to the myList data
+    app.get("/tourSpot/:email", async (req, res) => {
+      const result = await tourSpotCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
